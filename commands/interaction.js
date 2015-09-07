fec8644bdf675d833948c174e7b4e00d77363163
justastranger <justastrangerlica@gmail.com>
2015-09-07 18:04:29 -0500

    Added the ability to change the bot's nick The new nick is not remembered, but could be saved in a config file

--- commands/interaction.js
+++ commands/interaction.js
@@ -5,9 +5,11 @@ function say(from, channel, args){
 	}
 	global.bot.say(channel, from + ": " + args);
 }
+
 function asay(from, channel, args){
 	global.bot.say(channel, args);
 }
+
 function action(from, channel, args){
 	if(args == "") {
 		global.bot.say(channel, "Do what?");
@@ -15,6 +17,7 @@ function action(from, channel, args){
 	}
 	global.bot.action(channel, args);
 }
+
 function tell(from, channel, args){
 	if(args == "") {
 		global.bot.say(channel, "Do what?");
@@ -29,15 +32,23 @@ function tell(from, channel, args){
 	else global.tells[target].push(t);
 	global.bot.say(channel, "Okay, I'll tell that to "+target)
 }
+
 function help(from, channel, args){
 	global.bot.say(channel, "The supported commands are: "+ Object.keys(global.commands).join(", "));
 }
+
 function lewd(from, channel, args){
 	global.bot.say(channel, "http://i.imgur.com/mgveyIr.png");
 }
+
+function nick(from, channel, args){
+	global.bot.nick(args);
+}
+
 global.declareCommand(asay, ["asay"], "owner");
 //global.declareCommand(lewd, ["lewd"], "anyone");
 global.declareCommand(help, ["help", "commands"], "anyone");
 global.declareCommand(say, ["say"], "anyone");
 global.declareCommand(action, ["do", "action"], "anyone");
 global.declareCommand(tell, ["tell"], "anyone");
+global.declareCommand(nick, ["nick", "name"], "owner");
