f134704ae2649aea213d9c775129d577db38791d
justastranger <justastrangerlica@gmail.com>
2015-09-07 16:48:40 -0500

    Added a help command and a raw say command

--- commands/interaction.js
+++ commands/interaction.js
@@ -5,6 +5,9 @@ function say(from, channel, args){
 	}
 	global.bot.say(channel, from + ": " + args);
 }
+function asay(from, channel, args){
+	global.bot.say(channel, args);
+}
 function action(from, channel, args){
 	if(args == "") {
 		global.bot.say(channel, "Do what?");
@@ -26,7 +29,15 @@ function tell(from, channel, args){
 	else global.tells[target].push(t);
 	global.bot.say(channel, "Okay, I'll tell that to "+target)
 }
-
+function help(from, channel, args){
+	global.bot.say(channel, "The supported commands are: "+ Object.keys(global.commands).join(", "));
+}
+function lewd(from, channel, args){
+	global.bot.say(channel, "http://i.imgur.com/mgveyIr.png");
+}
+global.declareCommand(asay, ["asay"], "owner");
+global.declareCommand(lewd, ["lewd"], "anyone");
+global.declareCommand(help, ["help", "commands"], "anyone");
 global.declareCommand(say, ["say"], "anyone");
 global.declareCommand(action, ["do", "action"], "anyone");
 global.declareCommand(tell, ["tell"], "anyone");
