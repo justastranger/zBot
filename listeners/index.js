be8b4239cdb4902711ae3aa67973fece9c4e312f
justastranger <justastrangerlica@gmail.com>
2017-07-30 01:47:39 -0500

    Removed commented out area since that was already taken care of

--- listeners/index.js
+++ listeners/index.js
@@ -9,22 +9,3 @@ exports.lewd = require("./lewd.js");
 exports.log = require("./log.js");
 exports.seen = require("./seen.js");
 exports.tell = require("./tell.js");
-
-// TODO move this part somewhere else
-// since the exports are brought together, do a for (var listener in listeners) bot.addListener(listener.type, listener.listener);???
-//
-// global.bot.addListener(lewd.type, lewd.listener);
-// global.bot.addListener(tell.type, tell.listener);
-// global.bot.addListener(seen.type, seen.listener);
-// global.bot.addListener(log.type, log.listener);
-// global.bot.addListener(command.type, command.listener);
-// global.bot.addListener(error.type, error.listener);
-// global.bot.addListener(invite.type, invite.listener);
-
-// exports.command = command;
-// exports.error = error;
-// exports.invite = invite;
-// exports.lewd = lewd;
-// exports.log = log;
-// exports.seen = seen;
-// exports.tell = tell;
